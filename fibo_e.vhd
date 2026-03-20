a36f7301bfe887db432685ddbb0670746a87ed69
Your Name <your.email@example.com>
2026-03-20 17:23:59 +0100

    initial commit

---
+++ fibo_e.vhd
@@ -0,0 +1,36 @@
+library ieee;
+
+
+use ieee.std_logic_1164.all;
+use IEEE.STD_LOGIC_signed.ALL;
+
+
+entity fibo_e is
+	port(clk, en, rst : in std_logic;
+			N : in std_logic_vector(7 downto 0);
+			data_out: out std_logic_vector(15 downto 0):=x"0001");
+end entity;
+
+architecture fibo_a of fibo_e is
+	
+begin	
+	process
+	variable previous_value, next_value : std_logic_vector(15 downto 0):= x"0000";
+	variable current_value : std_logic_vector(15 downto 0):= x"0001";
+	
+	begin
+	wait until clk'event and clk='0';
+	if rst = '0' then
+		previous_value := x"0000";
+		next_value := x"0000";
+		current_value := x"0001";
+	end if;
+	if en = en then
+		next_value := current_value + previous_value;
+		previous_value := current_value;
+		current_value := next_value;
+		data_out <= current_value;
+	end if;
+	end process;
+	
+end architecture;
---
+++ seven_seg_e.vhd
@@ -0,0 +1,40 @@
+library ieee;
+
+
+use ieee.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+
+entity seven_seg_e is
+	port(A: in std_logic_vector(3 downto 0);
+			S: out std_logic_vector(6 downto 0));
+end entity;
+
+architecture seven_seg_a of seven_seg_e is
+begin
+process(A)
+begin
+	case A is 
+		when X"0"	=> S <= b"1000000";		-- 0
+		when X"1"	=> S <= b"1111001";		-- 1
+		when X"2"	=> S <= b"0100100";		-- 2
+		when X"3"	=> S <= b"0110000";		-- 3
+		when X"4"	=> S <= b"0011001";		-- 4
+		when X"5"	=> S <= b"0010010";		-- 5
+		when X"6"	=> S <= b"0000010";		-- 6
+		when X"7"	=> S <= b"1111000";		-- 7
+		when X"8"	=> S <= b"0000000";		-- 8
+		when X"9"	=> S <= b"0010000";		-- 9
+		when X"A"	=> S <= b"0001000";		-- A
+		when X"B"	=> S <= b"0000011";		-- B
+		when X"C"	=> S <= b"1000110";		-- C
+		when X"D"	=> S <= b"0100001";		-- D
+		when X"E"	=> S <= b"0000110";		-- E
+		when X"F"	=> S <= b"0001110";		-- F
+		--when X"10"	=> S <= b"0000000";		-- DOT
+		--when X"11"	=> S <= b"1111111";		-- NONE
+		--when X"12"	=> S <= b"0111111";		-- NEG
+		--when others => S <= b"1111111";		-- NONE
+	end case;
+	end process;
+end architecture;
